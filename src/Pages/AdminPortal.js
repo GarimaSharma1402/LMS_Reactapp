71a906686b2e2b1ab76314d83bc4ec3554c9cf65
Subhradeep-Maiti <144246199+Subhradeep-Maiti@users.noreply.github.com>
2023-09-18 20:59:56 +0530

    Update AdminPortal.js

--- src/Pages/AdminPortal.js
+++ src/Pages/AdminPortal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { useNavigate } from 'react-router-dom';
+import './MyStyle.css';
 const navigate=useNavigate();
 function handleClick(event)
 {
@@ -9,15 +10,15 @@ function handleClick(event)
 }
 function AdminPortal  (){
     return(
-        <div style={{display:"flex",alignItems:"center",justifyContent:"center",height:"100%"}}>
-            <h1>Welcome to Loan Management Application</h1>
-            <h3>Admin Dashboard</h3>
+        <center>
+            <h1 className="title">Welcome to Loan Management Application</h1><br></br>
+            <h3 className="title">Admin Dashboard</h3><br></br>
             <Stack direction="row" spacing={2}>
-                <Button>View Loans</Button>
-                <Button onSubmit={handleClick}>Apply For Loans</Button>
-                <Button>View Items Purchased</Button>
+                <Button className="btn1">View Loans</Button>
+                <Button className="btn1" onSubmit={handleClick}>Apply For Loans</Button>
+                <Button className="btn1">View Items Purchased</Button>
             </Stack>
-        </div>
+        </center>
     )
 }
 export default AdminPortal;
