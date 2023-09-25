abff51fbe45c221192bff01a68d251b3a2a518e9
Subhradeep-Maiti <144246199+Subhradeep-Maiti@users.noreply.github.com>
2023-09-25 09:31:57 +0530

    Update LoginwithToken.js

--- src/Pages/LoginwithToken.js
+++ src/Pages/LoginwithToken.js
@@ -46,21 +46,80 @@ const LoginwithToken = () =>{
     
     return (
    
-        <div>
-            <h3>Login using username and password</h3>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    Username: <input type="text" value={username} onChange={handleUsername} />
-                </div>
-                <div>
-                    Password: <input type="password" value={password} onChange={handlepwd} />
-                </div>
-                <div>
-                    <button type = "submit"> Login </button>
-                </div>
-                {Error && <div>Invalid Details</div>}
-            </form>
+        <center>
+  <title>Login Page</title>
+  <br></br><br></br>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+    crossOrigin="anonymous"
+  />
+  {/*Fontawesome CDN*/}
+  <link
+    rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+    integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+    crossOrigin="anonymous"
+  />
+  {/*Custom styles*/}
+  <link rel="stylesheet" type="text/css" href="styles.css" />
+  <div className="container">
+    <div className="d-flex justify-content-center h-100">
+      <div className="card">
+        <div className="card-header">
+          <h3>Sign In</h3>
         </div>
+        <div className="card-body">
+          <form>
+            <div className="input-group form-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <i className="fas fa-user" />
+                </span>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="username" value={username} onChange={handleUsername} />
+            </div>
+            <div className="input-group form-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <i className="fas fa-key" />
+                </span>
+              </div>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="password" value={password} onChange={handlepwd}
+              />
+            </div>
+            <div className="row align-items-center remember">
+              <input type="checkbox" />
+              Remember Me
+            </div>
+            <div className="form-group">
+              <input
+                type="submit"
+                defaultValue="Login"
+                className="btn float-right login_btn" onClick={handleSubmit}
+              />
+            </div>
+          </form>
+        </div>
+        <div className="card-footer">
+          <div className="d-flex justify-content-center links">
+            Don't have an account?<a href="http://localhost:3000/register">Sign Up</a>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</center>
+
+        
     )
 }
 export default LoginwithToken
